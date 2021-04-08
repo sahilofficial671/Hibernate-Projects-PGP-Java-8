2b2797fe02c41ff38d58b3f4a8f0b8c93ec30375
Sahil Bhatia <sahilofficial671@gmail.com>
2021-04-08 22:23:33 +0530

    Chapter 5 Exercise 1 Configured & completed

--- Chapter 5/Exercise 1/src/com/newtechbookstore/controller/AppServlet.java
+++ Chapter 5/Exercise 1/src/com/newtechbookstore/controller/AppServlet.java
@@ -32,66 +32,6 @@ public class AppServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-//		SessionFactory sessionfactory = configuration.buildSessionFactory();		
-//		Session session = sessionfactory.openSession();
-//		Transaction t = session.beginTransaction();
-//		
-//		// Save Book
-//		Book book =  new Book();
-//		book.setName("Twight Saga");
-//		book.setEdition("2.0");
-//		book.setCategory("techincal");
-//		Author author = new Author();
-//		author.setName("Sahil Bhatia");
-//		book.setAuthor(author);
-//		book.setPublishingYear(2021);
-//		book.setPrice(145);
-//		book.setQuantity(40);
-//		session.save(book);
-//		System.out.println(book);
-//		
-//		// Save Customer
-//		Customer customer = new Customer();
-//		customer.setUserName("sahil");
-//		customer.setFirstName("Sahil");
-//		customer.setLastName("Bhatia");
-//		customer.setGender("Male");
-//		try {
-//			customer.setDob(new SimpleDateFormat("YYYY-mm-dd").parse("1998-03-24"));
-//		} catch (ParseException e) {
-//			e.printStackTrace();
-//		}
-//		customer.setEmail("sahil@sahil.com");
-//		customer.setPhone("9560487757");
-//		customer.setPassword("sahil1234");
-//		
-//		// Save Customer's Addresss
-//		Address address = new Address();
-//		address.setLine_1("H No. 20, Faridabad");
-//		customer.setAddress(address);
-//		address.setCustomer(customer);
-//		System.out.println(customer);
-//		session.save(customer);
-//
-//		// Save Order
-//		Order order = new Order();
-//		order.setBookId(1);
-//		order.setCustomerId(1);
-//		order.setQuantity(10);
-//		order.setShippingAddress("H No. 20");
-//		order.setOrderedAt(new Timestamp(System.currentTimeMillis()));
-//		System.out.println(order);
-//		session.save(order);
-//		
-//		Book book = new BookDaoImpl().getBookById(1);
-//		System.out.println(book);
-//		System.out.println(new BookDaoImpl().decreaseBookQuantityBy(book, 10));
-//		System.out.println(new BookDaoImpl().getBookById(1));
-//		t.commit();
-//		session.close();
-		
 		request.getRequestDispatcher("pages/index.jsp").forward(request, response);
-		return ;
 	}
 }
