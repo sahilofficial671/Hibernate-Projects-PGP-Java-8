fd94505d7826d73bb3e465910a6785ab5a0c5f06
Sahil Bhatia <sahilofficial671@gmail.com>
2021-04-08 22:09:51 +0530

    Chapter 3 Exercise 2 configured & remove comments :rocket:

--- Chapter 3/Exercise 2/src/com/newtechbookstore/controller/AppServlet.java
+++ Chapter 3/Exercise 2/src/com/newtechbookstore/controller/AppServlet.java
@@ -27,28 +27,6 @@ public class AppServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Book book = new BookDaoImpl().getBookById(1);
-		System.out.println(book);
-		System.out.println(book.toJson());
-//		List<Book> bookList = HibernateUtil.getSessionFactory().openSession().createQuery("from Book where id = :bookId").setInteger("bookId", 1).list();
-//		System.out.println(bookList);
-//		Book book = bookList.get(0);
-//		return ;
-//		SessionFactory sessionfactory;
-//		Configuration configuration = new Configuration().configure("hibernate.cfg.xml");
-//		sessionfactory = configuration.buildSessionFactory();
-//		Session session = sessionfactory.openSession();
-//		Transaction t = session.beginTransaction();
-//		Book book =  new Book();
-//		book.setName("New Book");
-//		Author author = new Author();
-//		author.setName("Sahil Bhatia");
-//		book.setAuthor(author);
-//		System.out.println(book);
-//		session.save(book);
-//		t.commit();
-//		session.close();
 		request.getRequestDispatcher("pages/index.jsp").forward(request, response);
-//		return ;
 	}
 }
